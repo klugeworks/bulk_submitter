0396b5ebdab2c6240f69d6c679f11edc8cc42263
Paul DiOrio <none>
2015-03-03 11:43:32 -0800

    Initial skeleton of job submitter

--- scripts/submitter.py
+++ scripts/submitter.py
@@ -2,11 +2,11 @@
 
 import argparse
 import logging
-#import redis
+import redis
 import os
 import glob
 from urlparse import urlparse
-#from pycube.io.tokenizer_job_pb2 import TokenizerJobMessage
+from kluge_bs.io.tokenizer_job_pb2 import TokenizerJobMessage
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -35,8 +35,8 @@ def main():
     logger.info('Setting up connection to redis')
     redis_server = urlparse(args.redis_server)
     if redis_server.port is not None:
-        #r = redis.StrictRedis(host=redis_server.host, port=redis_server.port, db=0)
-        pass
+        redis_connection = redis.StrictRedis(host=redis_server.host, port=redis_server.port, db=0)
+
     logger.info('Connected to redis')
 
     files = glob.glob(os.path.join(args.audio_path, '*' + args.audio_ext))
@@ -44,11 +44,11 @@ def main():
         abs_path = os.path.abspath(file)
         basename = os.path.splitext(os.path.basename(abs_path))[0]
         logger.info("Found %s" % basename)
-        #job = TokenizerJobMessage()
-        #job.uid = basename
-        #job.audio_uri = abs_path
-        #logger.info('Submitting job for ' + basename)
-        #r.lpush(args.work_queue, job.SerializeToString())
+        job = TokenizerJobMessage()
+        job.uid = basename
+        job.audio_uri = abs_path
+        logger.info('Submitting job for ' + basename)
+        redis_connection.lpush(args.work_queue, job.SerializeToString())
 
 
 if __name__ == '__main__':
